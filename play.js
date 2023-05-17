892bb5f11cb3920911cef7a70df0d1876838a67b
Hossein <hossein.saafi@gmail.com>
2023-05-17 16:36:38 -0700

    Connect to the Server

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541, // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-   // incoming data from the server
-   conn.on("data", (data) => {
-    console.log("Server says:", data);
-    // Additional processing or actions with the incoming data
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
-
