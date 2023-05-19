6003b143b79991cde53e916452153c1bd6dcd227
Hossein <hossein.saafi@gmail.com>
2023-05-18 18:33:14 -0700

    Name command and Sending Name

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: "localhost", // IP address here,
     port: 50541, // PORT number here,
@@ -10,13 +10,33 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-   // incoming data from the server
-   conn.on("data", (data) => {
+  // incoming data from the server
+  conn.on("data", (data) => {
     console.log("Server says:", data);
-    // Additional processing or actions with the incoming data
   });
+  // handle successful connection
+  conn.on("connect", () => {
+    console.log("Successfully connected to the game server!");
+    
+
+    // sending name
+    conn.write("Name: MHS");
+    setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 50);
+  //   setInterval(() => {
+  //     conn.write("Move: down");
+  //   }, 50);
+  //   setInterval(() => {
+  //     conn.write("Move: left");
+  //   }, 50);
+  //   setInterval(() => {
+  //     conn.write("Move: right");
+  //   }, 50);
+          
+  // });
 
   return conn;
-};
+});
 
 module.exports = { connect };
