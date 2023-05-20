246534407ef2b11e82d7d5fde1bbc208bad01372
Hossein <hossein.saafi@gmail.com>
2023-05-19 20:50:13 -0700

    export the connect function using ES6 Shorthand syntax

--- play.js
+++ play.js
@@ -2,3 +2,11 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
