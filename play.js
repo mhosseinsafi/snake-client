f66e16d43fb4707ecc3a4020222fd908caeb089c
Hossein <hossein.saafi@gmail.com>
2023-05-21 19:01:09 -0700

    use the ES6 object shorthand notation

--- play.js
+++ play.js
@@ -1,20 +1,23 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function () {
- // \u0003 maps to ctrl+c input
-if (key === '\u0003') {
-  process.exit();
-}
-};
+setupInput();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+// const handleUserInput = function () {
+//  // \u0003 maps to ctrl+c input
+// if (key === '\u0003') {
+//   process.exit();
+// }
+// };
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
+//   return stdin;
+// };
