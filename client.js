686428136c3d0fa47d8afacf5361d21a89b9ad02
Hossein <hossein.saafi@gmail.com>
2023-05-21 18:39:12 -0700

    listening for user input via the keyboard

--- client.js
+++ client.js
@@ -21,18 +21,18 @@ const connect = function() {
 
     // sending name
     conn.write("Name: MHS");
-    setInterval(() => {
-      conn.write("Move: up");
-    }, 50);
-    setInterval(() => {
-      conn.write("Move: down");
-    }, 100);
-    setInterval(() => {
-      conn.write("Move: left");
-    }, 150);
-    setInterval(() => {
-      conn.write("Move: right");
-    }, 200);
+  //   setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 50);
+  //   setInterval(() => {
+  //     conn.write("Move: down");
+  //   }, 100);
+  //   setInterval(() => {
+  //     conn.write("Move: left");
+  //   }, 150);
+  //   setInterval(() => {
+  //     conn.write("Move: right");
+  //   }, 200);
           
   });
 
